34ca6aed22bcbbada0f064ca7c9fa2b978eb730a
Kyan <kyan@blackdrgn.nl>
2025-02-22 14:21:22 +0100

    Add powershell support to install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,18 @@
-#!/bin/bash
+echo \" <<'POWERSHELL_SCRIPT' >/dev/null # " | Out-Null
+if !(([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    echo "what are you doing?"
+} else {
+    echo "what the FUCK are you doing?"
+}
+<#
+POWERSHELL_SCRIPT
+
+set +o histexpand 2>/dev/null
 if [ "$EUID" -ne 0 ]; then
     echo "what are you doing?"
 else
     echo "what the FUCK are you doing?"
 fi
+case $- in *"i"*) cat /dev/stdin >/dev/null ;; esac
+exit
+#>
